a2bcda678e3be1beaca6decca84d77e21b45d982
Dinesh S <dineshindevelope@gmail.com>
2025-02-20 14:44:23 +0530

    frontend design

---
+++ client/src/components/EmptyTask.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const EmptyTask = () => {
+  return (
+    <div>
+      <div>
+        <div className="text-center mt-10 text-blue-700 font-semibold">
+          Your Task is Empty.. Add your task to Begin ..!
+        </div>
+        <div className="flex justify-center py-20">
+          <div className="loader">
+            <div className="box box0">
+              <div></div>
+            </div>
+            <div className="box box1">
+              <div></div>
+            </div>
+            <div className="box box2">
+              <div></div>
+            </div>
+            <div className="box box3">
+              <div></div>
+            </div>
+            <div className="box box4">
+              <div></div>
+            </div>
+            <div className="box box5">
+              <div></div>
+            </div>
+            <div className="box box6">
+              <div></div>
+            </div>
+            <div className="box box7">
+              <div></div>
+            </div>
+            <div className="ground">
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmptyTask;
--- client/src/pages/ReadTask.jsx
+++ client/src/pages/ReadTask.jsx
@@ -1,43 +1,114 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import EmptyTask from "../components/EmptyTask";
 
 const ReadTask = () => {
   const [data, setData] = useState([]);
+  const [username, setUsername] = useState("");
   const apiUrl = import.meta.env.VITE_BASE_URL;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalUpdateOpen, setIsModalUpdateOpen] = useState(false);
+
+  const [title, setTitle] = useState("");
+  const [discription, setDiscription] = useState("");
+  const [selectedTask, setSelectedTask] = useState(null);
+
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(`${apiUrl}/profile`, {
+        withCredentials: true,
+      });
+      setUsername(res.data.username);
+      console.log(res.data.username);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchTask = async () => {
+    try {
+      const res = await axios.get(`${apiUrl}/task`, {
+        withCredentials: true,
+      });
+      setData(res.data);
+      //console.log(res.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const handleDelete = async (id) => {
     try {
       await axios.delete(`${apiUrl}/task/${id}`, {
         withCredentials: true,
       });
+      setData(data.filter((task) => task._id !== id));
     } catch (error) {
       console.error(error);
     }
   };
 
-  useEffect(() => {
-    const fetchTask = async () => {
-      try {
-        const res = await axios.get(`${apiUrl}/task`, {
+  const addTask = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(
+        `${apiUrl}/task`,
+        { title, discription },
+        {
           withCredentials: true,
-        });
-        setData(res.data);
-        //console.log(res.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+        }
+      );
+      setIsModalOpen(false);
+      fetchTask();
+    } catch (error) {
+      console.error(error);
+      setIsModalOpen(false);
+    }
+  };
+  const openUpdateModel = (task) => {
+    setSelectedTask(task);
+    setTitle(task.title);
+    setDiscription(task.discription);
+    setIsModalUpdateOpen(true);
+  };
+
+  const updateTask = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.put(
+        `${apiUrl}/task/${selectedTask._id}`,
+        { title, discription },
+        {
+          withCredentials: true,
+        }
+      );
+      setIsModalUpdateOpen(false);
+      setSelectedTask(null);
+      fetchTask();
+    } catch (error) {
+      console.error(error);
+      setIsModalUpdateOpen(false);
+    }
+  };
+
+  useEffect(() => {
     fetchTask();
-  }, [handleDelete]);
+    fetchUser();
+  }, []);
   return (
-    <div className="px-3">
+    <div className="px-3 ">
       <div className="flex justify-between items-center mt-5">
         <button className="button">
-          <span className="text-xs flex">Welcome Dinesh Kumar</span>
+          <span className="text-xs flex">
+            WELCOME {username.toUpperCase() || "User"}
+          </span>
         </button>
 
         <div className="flex items-center space-x-4">
-          <button className="text-xs bg-gray-700 p-2 rounded text-white cursor-pointer hover:bg-gray-800">
+          <button
+            className="  text-xs bg-gray-700 p-2 rounded text-white cursor-pointer hover:bg-gray-800"
+            onClick={() => setIsModalOpen(true)}
+          >
             Add Task
           </button>
           <button className="text-xs bg-rose-500  p-2 rounded text-gray-100 cursor-pointer hover:bg-rose-600">
@@ -45,31 +116,148 @@ const ReadTask = () => {
           </button>
         </div>
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-5  sm:mt-10 relative">
-        {data.map((item, index) => {
-          return (
-            <div
-              className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 mb-5 sm:mb-10"
-              key={index}
-            >
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                {item.title}
-              </h5>
+      {isModalOpen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50 z-50 px-5">
+          <div className="bg-white rounded-lg shadow-md p-6 w-full max-w-md">
+            <div className="flex justify-between items-center border-b pb-3">
+              <h3 className="text-xl font-semibold">Add Task</h3>
+              <button
+                className="bg-red-600 p-1.5  text-white font-semibold text-lg rounded cursor-pointer"
+                onClick={() => setIsModalOpen(false)} // Close modal
+              >
+                ×
+              </button>
+            </div>
 
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                {item.discription}
-              </p>
+            <form className="space-y-4 mt-4" onSubmit={addTask}>
+              <div>
+                <label className="block mb-2 text-sm font-medium">
+                  Task Title
+                </label>
+                <input
+                  type="text"
+                  name="title"
+                  onChange={(e) => setTitle(e.target.value)}
+                  required
+                  className="w-full p-2 border rounded-lg"
+                  placeholder="Enter task title"
+                />
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium">
+                  Description
+                </label>
+                <textarea
+                  className="w-full p-2 border rounded-lg"
+                  name="discription"
+                  onChange={(e) => setDiscription(e.target.value)}
+                  required
+                  placeholder="Enter task description"
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                className="w-full  text-white p-2 rounded-lg  btn"
+              >
+                Add Task
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+      {data.length >= 1 ? (
+        <div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-5  sm:mt-10 relative">
+            {data.map((item) => {
+              return (
+                <div
+                  className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 mb-5 sm:mb-10"
+                  key={item._id}
+                >
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {item.title}
+                  </h5>
 
-              <div className="flex justify-between items-center">
-                <button className="btn">UPDATE</button>
-                <button className="btn" onClick={() => handleDelete(item._id)}>
-                  DELETE
-                </button>
+                  <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {item.discription}
+                  </p>
+
+                  <div className="flex justify-between items-center">
+                    <button
+                      className="btn"
+                      onClick={() => openUpdateModel(item)}
+                    >
+                      UPDATE
+                    </button>
+                    <button
+                      className="btn"
+                      onClick={() => handleDelete(item._id)}
+                    >
+                      DELETE
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          {isModalUpdateOpen && (
+            <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50 z-50 px-5">
+              <div className="bg-white rounded-lg shadow-md p-6 w-full max-w-md">
+                <div className="flex justify-between items-center border-b pb-3">
+                  <h3 className="text-xl font-semibold">Edit your Task</h3>
+                  <button
+                    className="bg-red-600 p-1.5  text-white font-semibold text-lg rounded cursor-pointer"
+                    onClick={() => setIsModalUpdateOpen(false)} // Close modal
+                  >
+                    ×
+                  </button>
+                </div>
+
+                <form className="space-y-4 mt-4" onSubmit={updateTask}>
+                  <div>
+                    <label className="block mb-2 text-sm font-medium">
+                      Task Title
+                    </label>
+                    <input
+                      type="text"
+                      name="title"
+                      value={title}
+                      onChange={(e) => setTitle(e.target.value)}
+                      required
+                      className="w-full p-2 border rounded-lg"
+                      placeholder="Enter task title"
+                    />
+                  </div>
+                  <div>
+                    <label className="block mb-2 text-sm font-medium">
+                      Description
+                    </label>
+                    <textarea
+                      className="w-full p-2 border rounded-lg"
+                      name="discription"
+                      onChange={(e) => setDiscription(e.target.value)}
+                      required
+                      value={discription}
+                      placeholder="Enter task description"
+                    ></textarea>
+                  </div>
+                  <button
+                    type="submit"
+                    className="w-full  text-white p-2 rounded-lg  btn"
+                  >
+                    Update Task
+                  </button>
+                </form>
               </div>
             </div>
-          );
-        })}
-      </div>
+          )}
+        </div>
+      ) : (
+        <div>
+          {" "}
+          <EmptyTask />{" "}
+        </div>
+      )}
     </div>
   );
 };
